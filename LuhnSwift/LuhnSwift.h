adb1341bd289e35f4fa7eda2da1d27607bfb9537
Kyle McAlpine <kyle@loot.io>
2016-03-25 09:58:21 +0000

    Initial commit

---
+++ LuhnSwift/LuhnSwift.h
@@ -0,0 +1,19 @@
+//
+//  LuhnSwift.h
+//  LuhnSwift
+//
+//  Created by Kyle McAlpine on 25/03/2016.
+//  Copyright © 2016 Loot Financial Services Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LuhnSwift.
+FOUNDATION_EXPORT double LuhnSwiftVersionNumber;
+
+//! Project version string for LuhnSwift.
+FOUNDATION_EXPORT const unsigned char LuhnSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LuhnSwift/PublicHeader.h>
+
+
---
+++ LuhnSwiftTests/LuhnSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  LuhnSwiftTests.swift
+//  LuhnSwiftTests
+//
+//  Created by Kyle McAlpine on 25/03/2016.
+//  Copyright © 2016 Loot Financial Services Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import LuhnSwift
+
+class LuhnSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
